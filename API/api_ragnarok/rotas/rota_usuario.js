0cb2bb961e9f0cf0cf9947169fa76744eb9ac7e4
Vincius Morais <18175296@CFP127ALUNO.LOCAL>
2019-08-27 14:12:17 -0300

    API sem o 'cep' na hora de criar o usuario 27/08 14:12

--- API/api_ragnarok/rotas/rota_usuario.js
+++ API/api_ragnarok/rotas/rota_usuario.js
@@ -2,31 +2,31 @@ const express = require("express");
 const router = express.Router();
 const sha512 = require("js-sha512").sha512;
 const Usuario = require("../models/Usuario");
-const CepUtils = require("../utils/CepUtils");
+//const CepUtils = require("../utils/CepUtils");
 
 router.post("/", (req, res)=>{
-    let { nome, email, senha, cep } = req.body;
+    let { nome, email, senha } = req.body;
 
     senha = sha512(senha);
 
-    CepUtils.getCoordenadas(cep, (coordendas)=>{
-        console.log(coordenadas)
-    })
+    // CepUtils.getCoordenadas(cep, (coordendas)=>{
+    //     console.log(coordenadas)
+    // })
 
-    // Usuario.create({
-    //     nome, email, senha,
-    //     is_bloqueado: false,
-    //     is_admin:false
+    Usuario.create({
+        nome, email, senha,
+        is_bloqueado: false,
+        is_admin:false
 
-    // }).then((usuario)=>{
-    //     //201 Criado
-    //     res.sendStatus(201).end();
+    }).then((usuario)=>{
+        //201 Criado
+        res.sendStatus(201).end();
 
-    // }).catch((err)=>{
-    //     console.log("Erro ao criar o usuario: " + err)
-    //     //400 Requisição Ruim
-    //     res.status(400).json(err).end();
-    // });
+    }).catch((err)=>{
+        console.log("Erro ao criar o usuario: " + err)
+        //400 Requisição Ruim
+        res.status(400).json(err).end();
+    });
 });
 
 module.exports = router;
