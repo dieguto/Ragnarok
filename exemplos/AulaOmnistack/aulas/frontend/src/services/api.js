34f499a53cc1b054b09d1cd7a1ce10e542eda446
Diego Augusto <diegoaugusto1606@gmai.com>
2019-09-03 16:26:15 -0300

    Alteração no menu WEB

--- exemplos/AulaOmnistack/aulas/frontend/src/services/api.js
+++ exemplos/AulaOmnistack/aulas/frontend/src/services/api.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 // chama o axios, e diz que base da URL vai ser http.... para quando mudar para
 // o servidor, é só mudar a base, pra que não precise alterar em todas as paginas
 const api = axios.create({
-    baseURL: 'http://localhost:3009'
+    baseURL: 'http://localhost:3107'
 });
 
 export default api;
