888798602aa006b9bf5c6855e6e2032572cf63a4
DIEGO AUGUSTO FARIAS SILVA <18175158@CFP127ALUNO.LOCAL>
2019-09-16 16:47:25 -0300

    Alterando o menu

--- WEB/ragnarok/src/components/Menu.js
+++ WEB/ragnarok/src/components/Menu.js
@@ -4,8 +4,14 @@ import { GiBrutalHelm } from 'react-icons/gi';
 
 class Menu extends Component{
 
+  
+
   render(){
+    let menu;
       return(
+
+        
+        
           <div className="header">
             <div className="navbar navbar-light bg-dark">
               <h1 className="navbar-brand text-warning">Ragnarok</h1>
@@ -15,9 +21,35 @@ class Menu extends Component{
                 </div>
                 
             </div>
-            
+            {/* Segundo menu */}
+            <div className="navbar navbar-light bg-dark">
+              <h1 className="navbar-brand text-warning">Ragnarok</h1>
+                
+                
+                <div className="btn-group">
+                <div>
+                    <span>Bem vindo, Diego</span>
+                    
+                    <button type="button" className="btn btn-secondary dropdown-toggle ml-3" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        
+                        <GiBrutalHelm/>
+                        {/* <p></p> */}
+                    </button>
+                    <div className="dropdown-menu dropdown-menu-right">
+                        {/* <button className="dropdown-item" type="button">Bem vindo, Diego</button> */}
+                        <button className="dropdown-item" type="button">Meus Anúncios</button>
+                        <button className="dropdown-item" type="button">Meus Jogos</button>
+                        <button className="dropdown-item" type="button">Configurações</button>
+                        <button className="dropdown-item" type="button">Sair</button>
+                    </div>
+                </div>
+                    
+            </div>
+            </div>
             
           </div>
+
+          
    );
   }
 
