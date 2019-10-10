566a4b227eaa09da2c11b6a571ad389d8194ff3f
Diego Augusto <diegoaugusto1606@gmail.com>
2019-10-10 10:22:10 -0300

    Slider dinamico + alteração front-WEB

---
+++ WEB/ragnarok/src/assets/images.js
@@ -0,0 +1,5 @@
+export const images = [
+    "https://images.unsplash.com/photo-1490810194309-344b3661ba39?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1290&q=80",
+    "https://images.unsplash.com/photo-1495954222046-2c427ecb546d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80",
+    "https://images.unsplash.com/photo-1489257712451-3a66755ca19c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+]
--- WEB/ragnarok/src/components/Main.js
+++ WEB/ragnarok/src/components/Main.js
@@ -3,6 +3,8 @@ import Banner from './Banner';
 import MainView from './MainView.js';
 import { connect } from 'react-redux';
 import agent from '../agent'
+import { images } from '../assets/images';
+import { Gallery, GalleryImage} from 'react-gesture-gallery';
 
 const Promise = global.Promise;
 
@@ -15,17 +17,55 @@ const mapDispatchToProps = dispatch => ({
   dispatch({ type: 'HOME_PAGE_LOADED', payload }),
 });
 
+// const [index, setIndex] = React.useState(0)
+const INITIAL_INDEX = 0
+function Slide() {
+  const [index, setIndex] = React.useState(INITIAL_INDEX)
 
+  React.useEffect(() => {
+    const interval = setInterval( () => {
+      if(index === images.length - 1){
+        setIndex(INITIAL_INDEX)
+      }else{
+        setIndex(index + 1)
+      }
+     
+    }, 2500)
+    return () => clearInterval(interval)
+  }, [index])
+
+
+  return (
+    <Gallery
+    style= {{
+      heigth: '100vh',
+      width: '100vw'
+    }}
+    index={index}
+    onRequestChange={i => {
+      setIndex(i)
+    }}
+    >
+    {images.map(image => (
+      <GalleryImage objectFit="contain" src={image} />
+    ))}
+  </Gallery>
+  )
+}
 
 class Main extends React.Component {
+ 
 
   componentDidMount(){
       this.props.onLoad(agent.Anuncios.all());
   }
 
     render() {
+     
       return (
         <div className="home-page">
+
+        <Slide></Slide>
   
           {/* <Banner appName={this.props.appName} /> */}
   
