8333fef2965a214eb489531146c7a71e4282c219
Diego Augusto <diegoaugusto1606@gmail.com>
2019-10-15 11:17:53 -0300

    corrigindo as bibliotecas

--- WEB/ragnarok/src/components/Main.js
+++ WEB/ragnarok/src/components/Main.js
@@ -19,39 +19,39 @@ const mapDispatchToProps = dispatch => ({
 
 // const [index, setIndex] = React.useState(0)
 const INITIAL_INDEX = 0
-function Slide() {
-  const [index, setIndex] = React.useState(INITIAL_INDEX)
+// function Slide() {
+//   const [index, setIndex] = React.useState(INITIAL_INDEX)
 
-  React.useEffect(() => {
-    const interval = setInterval( () => {
-      if(index === images.length - 1){
-        setIndex(INITIAL_INDEX)
-      }else{
-        setIndex(index + 1)
-      }
+//   React.useEffect(() => {
+//     const interval = setInterval( () => {
+//       if(index === images.length - 1){
+//         setIndex(INITIAL_INDEX)
+//       }else{
+//         setIndex(index + 1)
+//       }
      
-    }, 2500)
-    return () => clearInterval(interval)
-  }, [index])
+//     }, 2500)
+//     return () => clearInterval(interval)
+//   }, [index])
 
 
-  return (
-    <Gallery
-    style= {{
-      heigth: '100vh',
-      width: '100vw'
-    }}
-    index={index}
-    onRequestChange={i => {
-      setIndex(i)
-    }}
-    >
-    {images.map(image => (
-      <GalleryImage objectFit="contain" src={image} />
-    ))}
-  </Gallery>
-  )
-}
+//   return (
+//     <Gallery
+//     style= {{
+//       heigth: '100vh',
+//       width: '100vw'
+//     }}
+//     index={index}
+//     onRequestChange={i => {
+//       setIndex(i)
+//     }}
+//     >
+//     {images.map(image => (
+//       <GalleryImage objectFit="contain" src={image} />
+//     ))}
+//   </Gallery>
+//   )
+// }
 
 class Main extends React.Component {
  
@@ -65,7 +65,7 @@ class Main extends React.Component {
       return (
         <div className="home-page">
 
-        <Slide></Slide>
+        {/* <Slide></Slide> */}
   
           {/* <Banner appName={this.props.appName} /> */}
   
