bb030f0fce8b8a00995b1fbc0d9f9091995d5831
DIEGO AUGUSTO FARIAS SILVA <18175158@CFP127ALUNO.LOCAL>
2019-10-25 17:49:57 -0300

    arrumei o menu, preciso consertar o cadastro do anuncio

--- WEB/ragnarok2/src/App.js
+++ WEB/ragnarok2/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './css/marketing.css'
 import './css/bootstrap.min.css'
+import Menu from './components/Header';
 
 function App() {
   return (
     <div className="App">
-     
+        <Menu></Menu>
     </div>
   );
 }
---
+++ WEB/ragnarok2/src/components/Header.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router';
+import { GiBrutalHelm } from 'react-icons/gi';
+
+export default class Menu extends Component{
+    render(){
+        if(localStorage.getItem('token') === null){
+            return (
+                <div className="header">
+                <div className="navbar navbar-light bg-dark">
+                 
+          
+                  <Link to="/" className="navbar-brand">
+                    <h1 className="navbar-brand text-warning">
+                      Ragnarok
+                    </h1>
+                  </Link>
+                    <div className="form-inline">
+                        <Link to="/login" className="btn btn-outline-warning ">Entrar</Link>
+                        <Link to="/cadastro" className="btn btn-outline-warning ml-3">Cadastro</Link>
+                    </div>
+                    
+                </div>
+                </div>
+              )
+        }else{
+            return (
+                <div className="header">
+              
+                       {/* Segundo menu */}
+                       <div className="navbar navbar-light bg-dark">
+                         <h1 className="navbar-brand text-warning">
+                           Ragnarok
+                         </h1>
+                        
+                        
+                           <div className="btn-group">
+                           <div className="form-inline mr-3">
+                             <Link to="/anuncios" className="btn btn-outline-warning ">Novo Anúncio</Link>
+                           </div>
+                           <div>
+                           <Link
+                              
+                              className="nav-link">
+                              Usuário
+                            </Link>
+                               {/* <span>Bem vindo, Diego</span> */}
+                            
+                               <button type="button" className="btn btn-secondary dropdown-toggle ml-3" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                  
+                                   <GiBrutalHelm/>
+                               </button>
+                               <div className="dropdown-menu dropdown-menu-right">
+                                   {/* <button className="dropdown-item" type="button">Bem vindo, Diego</button> */}
+                                   <button className="dropdown-item" type="button">Meus Anúncios</button>
+                                   <button className="dropdown-item" type="button">Meus Jogos</button>
+                                   <button className="dropdown-item" type="button">Configurações</button>
+                                   <button className="dropdown-item" type="button">Sair</button>
+                               </div>
+                           </div>
+                              
+                      </div>
+                      </div>
+                      
+                     </div>
+          
+              )
+        }
+    }
+}
+
