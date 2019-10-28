8f1c65d00cffa224fcbc08177e396a4b27e35ed1
VINICIUS OLIVEIRA DE ALMEIDA <18175204@CFP127ALUNO.LOCAL>
2019-10-28 16:30:01 -0300

    Css atualizado

--- WEB/ragnarok2/src/components/CadastroJogo.js
+++ WEB/ragnarok2/src/components/CadastroJogo.js
@@ -21,26 +21,26 @@ class RetornaJogo extends Component {
       <div className="container">
         <form onSubmit={this.pesquisa.bind(this)}>
           <div className="form-group">
-            <label for="">Digite o nome do jogo:</label>
-            <input type="text" className="form-control" id=""  placeholder="God of War" ref={input => this.jogoPesquisado = input} />
+            <label for=""className="texto-laranja">Digite o nome do jogo:</label>
+            <input type="text" className="form-control texto-laranja" id=""  placeholder="God of War" ref={input => this.jogoPesquisado = input} />
             <input type="submit" value="pesquisar" />
           </div>
         </form>
-          
-    
         
+        <div class="container caixa-jogo">
           <div className="row">
             <div className="col-4">
 
-                <div className="row"><h5 className="card-title">God of War:</h5></div>
-                <div className="row"><button type="button" className="btn btn-confirmar">Clique aqui para confirmar</button></div>
+                <div className="row"><h5 className="card-title texto-laranja">God of War:</h5></div>
+                <div className="row"><button type="button" className="btn-confirmar">Clique aqui para confirmar</button></div>
               
               
             </div>
             <div className="col-8">
               
             </div>
-          </div>                  
+          </div>      
+        </div>              
     
       </div>
     )
@@ -66,7 +66,7 @@ class SelectGenero extends Component {
     render(){
         return(
             <div className="form-group">
-                <label for="">Gênero:</label>
+                <label for="" className="texto-laranja">Gênero:</label>
                 <select className="custom-select">
                     {
                         this.state.generos.map(
@@ -101,7 +101,7 @@ class SelectConsole extends Component {
   render(){
     return(
       <div className="form-group">
-        <label for="">Console do Jogo:</label>
+        <label for="" className="texto-laranja">Console do Jogo:</label>
           <select className="custom-select">
                     {
                         this.state.consoles.map(
@@ -133,25 +133,37 @@ export default class CadastroJogo extends Component {
             <div id="container">
 
       <h1 className="titulo-cadastro-anuncio">Cadastro de Anuncios</h1>
+      <div class="row">
+        <div class="col-3.5 mr-auto ml-auto"><hr class="accent-2 mb-4 mt-0 d-inline-block mx-auto linha-titulo-jogo"/></div>
+      </div>
       <div id="container-cadastro-anuncio">
-        <form class="form-cadastro" onSubmit={console.log("teste enviar o form de fora")}>
+        <form className="form-cadastro" onSubmit={console.log("teste enviar o form de fora")}>
           <div className="form-group">
-            <label for="">Titulo:</label>
+            <label for="" className="texto-laranja">Titulo:</label>
             <input type="text" className="form-control" id="" placeholder="Digite o titulo do produto"/>
           </div>
           <div className="form-group">
-            <label for="">Descrição:</label>
+            <label for="" className="texto-laranja">Descrição:</label>
             <input type="text" className="form-control" id="" placeholder="Descreva seu produto"/>
           </div>
           <SelectGenero></SelectGenero>
           <SelectConsole></SelectConsole>
         
           <div className="form-group">
-            <label for="">Preço:</label>
+            <label for="" className="texto-laranja">Preço:</label>
             <input type="number" className="form-control" id="" placeholder="Digite o preço do produto"/>
           </div>
           <RetornaJogo/>  
-          <button type="submit" className="btn btn-outline-warning">Cadastrar</button>
+          <div class="form-group">
+            <div class="custom-file">
+              <input type="file" class="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01"/>
+              <label class="custom-file-label" for="">Selecione a foto do produto que deseja anunciar</label>
+            </div>
+          </div>
+          <div class="form-group">
+            <div id="" class="background-333333 mr-auto ml-auto caixa_imagem"></div>
+          </div>        
+          <button type="submit" className="btn btn-outline-warning btn-cadastro">Cadastrar</button>
         </form>        
       </div>
       
