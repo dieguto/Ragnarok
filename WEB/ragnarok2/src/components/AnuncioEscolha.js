aadb89f5c7e0d6a2221f2c2b312aa481e5ea77f6
VINICIUS OLIVEIRA DE ALMEIDA <18175204@CFP127ALUNO.LOCAL>
2019-10-31 16:32:58 -0300

    design escolha anuncio

--- WEB/ragnarok2/src/components/AnuncioEscolha.js
+++ WEB/ragnarok2/src/components/AnuncioEscolha.js
@@ -9,42 +9,42 @@ import ImgTroca from '../assets/troca.png';
 export default class AnuncioEscolha extends Component {
     render(){
         return(
-            <div id="container">
-            <h1 className="titulo-cadastro-anuncio">O que deseja anunciar?</h1>
-            <div className="row">
-                <div className="col-5 mr-auto ml-auto">
-                    <hr className="accent-2 mb-4 mt-0 d-inline-block mx-auto linha-titulo"/>
-            </div>
-            </div>
-            <div id="container_escolha">
-                <div className="row">
-                    <div className="col-sm-5 ml-auto mr-auto card card-escolha background-333333">
-                        <div className="card-body body-card-escolha">
-                            <img src={ImgJogo} className="img-thumbnail background-333333 imagem-escolha"/>
-                        </div>
-                        <div className="card-footer text-center background-333333 texto-laranja footer-card-escolha">Jogo</div>
+            <>
+                <div id="container">
+                    <h1 className="titulo-cadastro-anuncio">O que deseja anunciar?</h1>
+                    <div class="row">
+                        <div class="col-3.5 mr-auto ml-auto"><hr class="accent-2 mb-4 mt-0 d-inline-block mx-auto linha-titulo-jogo"/></div>
                     </div>
-                    <div className="col-sm-5 ml-auto mr-auto card card-escolha background-333333">
-                        <div className="card-body body-card-escolha">
-                            <img src={ImgAcessorio} className="img-thumbnail background-333333 imagem-escolha"/>
+                    <div id="container_escolha">
+                        <div className="row">
+                            <div className="col-sm-5 ml-auto mr-auto card card-escolha background-333333">
+                                <div className="card-body body-card-escolha">
+                                    <img src={ImgJogo} className="img-thumbnail background-333333 imagem-escolha"/>
+                                </div>
+                                <div className="card-footer text-center background-333333 texto-laranja footer-card-escolha">Jogo</div>
+                            </div>
+                            <div className="col-sm-5 ml-auto mr-auto card card-escolha background-333333">
+                                <div className="card-body body-card-escolha">
+                                    <img src={ImgAcessorio} className="img-thumbnail background-333333 imagem-escolha"/>
+                                </div>
+                                <div className="card-footer text-center background-333333 texto-laranja footer-card-escolha">Acessorio</div>
+                            </div>
+                            <div className="col-sm-5 ml-auto mr-auto card card-escolha background-333333">
+                                <div className="card-body body-card-escolha">
+                                    <img src={ImgConsole} className="img-thumbnail background-333333 imagem-escolha"/>
+                                </div>
+                                <div className="card-footer text-center background-333333 texto-laranja footer-card-escolha">Console</div>
+                            </div>
+                            <div className="col-sm-5 ml-auto mr-auto card card-escolha background-333333">
+                                <div className="card-body body-card-escolha">
+                                    <img src={ImgTroca} className="img-thumbnail background-333333 imagem-escolha"/>
+                                </div>
+                                <div className="card-footer text-center background-333333 texto-laranja footer-card-escolha">Troca</div>
+                            </div>
                         </div>
-                        <div className="card-footer text-center background-333333 texto-laranja footer-card-escolha">Acessorio</div>
-                    </div>
-                    <div className="col-sm-5 ml-auto mr-auto card card-escolha background-333333">
-                        <div className="card-body body-card-escolha">
-                            <img src={ImgConsole} className="img-thumbnail background-333333 imagem-escolha"/>
-                        </div>
-                        <div className="card-footer text-center background-333333 texto-laranja footer-card-escolha">Console</div>
-                    </div>
-                    <div className="col-sm-5 ml-auto mr-auto card card-escolha background-333333">
-                        <div className="card-body body-card-escolha">
-                            <img src={ImgTroca} className="img-thumbnail background-333333 imagem-escolha"/>
-                        </div>
-                        <div className="card-footer text-center background-333333 texto-laranja footer-card-escolha">Troca</div>
                     </div>
                 </div>
-            </div>
-        </div>
+            </>
         ) 
     }
 }
