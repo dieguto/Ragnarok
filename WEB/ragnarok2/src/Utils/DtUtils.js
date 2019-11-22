35f2fa2f1c2d7a2e3857d9eba5cc0b84cc6eef7d
DIEGO AUGUSTO FARIAS SILVA <18175158@CFP127ALUNO.LOCAL>
2019-11-22 17:58:34 -0200

    Chat pegando 70%

---
+++ WEB/ragnarok2/src/Utils/DtUtils.js
@@ -0,0 +1,82 @@
+class DtUtils {
+
+   static addZero(num){
+      const num_str = num + "";
+
+      if(num_str.length > 1){
+         return num_str;
+      } else {
+         return "0" + num_str;
+      }
+   }
+
+   static convMes(mes){
+      return this.addZero(mes + 1);
+   }
+
+   static getInfoDtJson(dt_string){
+
+      const dt = new Date(dt_string || Date.now());
+      
+      const segundos = this.addZero(dt.getSeconds());
+
+      const minutos = this.addZero(dt.getMinutes());
+      
+      const horas = this.addZero(dt.getHours());
+
+      const hora= horas + ":" + minutos;
+
+      const hora_completa = horas + ":" + minutos + ":" + segundos;
+
+      const dia  = this.addZero(dt.getDate());
+
+      const mes = this.convMes(dt.getMonth());
+
+      const ano = dt.getFullYear() + "";
+
+      const dt_banco = ano + "-" + mes + "-" + dia;
+
+      const data = dia + "/" + mes + "/" + ano[2] + ano[3];
+
+      const data_completa = dia + "/" + mes + "/" + ano;
+
+      const millis = dt.getTime();
+
+      const info_dt = { 
+         data, segundos, minutos, horas, hora, hora_completa, 
+         dia, mes, ano, dt_banco, data, data_completa, 
+         millis
+      };
+
+      return info_dt;
+   }
+
+   static getDtConf(info_data){
+
+      const agora = this.getInfoDtJson();
+
+      const dif_millis = agora.millis - info_data.millis;
+
+      if(dif_millis < 86400000){
+         info_data.data = "Hoje";
+
+      } else if (dif_millis >= 86400000 && dif_millis < 172800000) {
+         info_data.data = "Ontem";
+
+      }
+
+      return { data: info_data.data, hora: info_data.hora, string: info_data.data + " ás " + info_data.hora };
+   }
+
+   static getDt(dt_string){
+      
+      const info_data = this.getInfoDtJson(dt_string);
+
+      const dt_conf = this.getDtConf(info_data);
+
+      return dt_conf;
+   }
+}
+
+export default DtUtils;
+//module.exports = DtUtils;
